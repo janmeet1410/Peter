f19b01ec8b432989e30daf45ae58106ce433345a
janmeet1410 <padiyajanmeet6@gmail.com>
2025-10-31 01:01:52 +0530

    updated

--- Source Code/GBIIntranet/src/webparts/socialMedia/components/SocialMedia.tsx
+++ Source Code/GBIIntranet/src/webparts/socialMedia/components/SocialMedia.tsx
@@ -29,10 +29,10 @@ export default class SocialMedia extends React.Component<ISocialMediaProps, ISoc
             {/* <iframe style={{ border: "none" }} height={"420px"} width={"100%"} data-tweet-url="https://www.linkedin.com/company/netuno-usa-inc" src="data:text/html;charset=utf-8,%3Ca%20class%3D%22twitter-timeline%22%20href%3D%22https%3A//twitter.com/iZOOlogic%3Fref_src%3Dtwsrc%255Etfw%22%3ETweets%20by%20iZOOlogic%3C/a%3E%0A%3Cscript%20async%20src%3D%22https%3A//platform.twitter.com/widgets.js%22%20charset%3D%22utf-8%22%3E%3C/script%3E%0A"></iframe> */}
             <div className="ms-Grid">
               <div className="ms-Grid-row">
-                <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6">
+                <div className="ms-Grid-col ms-sm6 ms-md12 ms-lg12">
                   <InstagramEmbed />
                 </div>
-                <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6">
+                {/* <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6">
                   <div className="ms-Grid-row">
                     {this.state.allVideos.length > 0 &&
                       this.state.allVideos.slice(0, 2).map((ele, ind) => (
@@ -49,23 +49,9 @@ export default class SocialMedia extends React.Component<ISocialMediaProps, ISoc
                         </video>
                       </div>
                     )}
-                    {/* <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6">
-                      <video controls>
-                        <source src="https://bullioninternational.sharepoint.com/sites/intranet/Videos/video1.mp4" type="video/mp4" />
-                      </video>
-                    </div>
-                    <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6">
-                      <video controls>
-                        <source src="https://bullioninternational.sharepoint.com/sites/intranet/Videos/video1.mp4" type="video/mp4" />
-                      </video>
-                    </div>
-                    <div className="ms-Grid-col ms-sm12 ms-md12 ms-lg12">
-                      <video controls>
-                        <source src="https://bullioninternational.sharepoint.com/sites/intranet/Videos/video1.mp4" type="video/mp4" />
-                      </video>
-                    </div> */}
+                   
                   </div>
-                </div>
+                </div> */}
                 {/* <div className="ms-Grid-col ms-sm12 ms-md12 ms-lg12">
                   <FacebookPageEmbed />
                 </div> */}
